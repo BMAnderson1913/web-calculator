d596c1c21cec07ccb61ed19e31aa63267d89dab4
Brittany Anderson <brittanyanderson@Brittanys-MacBook-Air.local>
2020-03-03 17:39:52 -0500

    Added new functions to web calculator

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-function myFunction()
-{
-var y = document.getElementById("txt1").value;
-var z = document.getElementById("txt2").value;
-var x = parseInt(y) + parseInt(z);
-document.getElementById("calc").innerHTML = x;
+function calculate() {
+    var num1 = document.getElementById("num1").value
+    var num2 = document.getElementById("num2").value
+    var selector = document.getElementById("math")
+
+    var math = selector.options[selector.selectedIndex].value
+
+
+    switch (math) {
+        case "addition":
+            var answer = parseInt(num1) + parseInt(num2)
+            break;
+        case "subtract":
+            var answer = parseInt(num1) - parseInt(num2)
+            break;
+        case "multiply":
+            var answer = parseInt(num1) * parseInt(num2)
+            break;
+        case "divide":
+            var answer = parseInt(num1) / (num2)
+            break;
+        default:
+            alert("Error")
+    }
+    document.getElementById("answer").innerHTML = answer
 }
